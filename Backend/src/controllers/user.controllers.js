2682d2a52df0d076924cc82d612a5e2bbdcb1e3a
54ntu <chaudharysantaram8@gmail.com>
2025-01-07 16:15:58 +0545

    apiresponse and apierror services are created

--- Backend/src/controllers/user.controllers.js
+++ Backend/src/controllers/user.controllers.js
@@ -1,7 +1,10 @@
 class UserController {
 
     async userRegister(req,res){
-        
+        const {username,email,password,phone_no,address,role}= req.body
+        if(!username || !email || !password || !phone_no || !address || !role){
+            
+        }
 
     }
 }
